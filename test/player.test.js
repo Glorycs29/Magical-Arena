15cfe1a0ff1e141ef9f8b4a7d3e1210bc7f17f30
Glory Gupta <prikarshglory@gmail.com>
2024-06-29 00:42:24 +0530

    player.test.js is used to check the correct initializationof attributes and correct functioning of isAlive function.

--- test/player.test.js
+++ test/player.test.js
@@ -1,1 +1,19 @@
-// const { expect } = require('chai');
+const { expect } = require('chai');
+const Player = require('../src/player');
+
+describe('Player', () =>{
+    it('should initialize with correct attributes', () =>{
+        const player = new Player('Player A', 50, 5, 10);
+        expect(player.name).to.equal('Player A');
+        expect(player.health).to.equal(50);
+        expect(player.strength).to.equal(5);
+        expect(player.attack).to.equal(10);
+    });
+
+    it('should correctly identify if player is alive', () =>{
+        const player = new Player('Player A', 50, 5, 10);
+        expect(player.isAlive()).to.be.true;
+        player.health = 0;
+        expect(player.isAlive()).to.be.false;
+    });
+});
