7c14dcaacf2c38f8e3f8efff764f1fdb1ec4fc8f
Glory Gupta <prikarshglory@gmail.com>
2024-06-29 00:49:43 +0530

    dice.test.js is used for testing whether the value showed up on dice is within 1 and 6 both inclusive

---
+++ test/dice.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const Dice = require('../src/dice');
+
+describe('Dice', () =>{
+    it('should return a value between 1 and 6', () =>{
+        for(let i=0; i<100; i++){
+            const roll = Dice.roll();
+            expect(roll).to.be.within(1,6);
+        }
+    });
+});
