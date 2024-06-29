90a78a2e76fac111e666430a6133fad7dc04b1b0
Glory Gupta <prikarshglory@gmail.com>
2024-06-29 22:21:27 +0530

    Corrected the logic of arena.js and arena.test.js, the checking point must be whether anyone is dead or not(end of fight)

--- src/arena.js
+++ src/arena.js
@@ -1,13 +1,13 @@
 import Dice from '../src/dice.js';
-export default
-class Arena{
-    constructor(player1, player2){
+
+export default class Arena {
+    constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    startFight(){
-        while(this.player1.isAlive() && this.player2.isAlive()){
+    startFight() {
+        while (this.player1.isAlive() && this.player2.isAlive()) {
             this.fightRound();
         }
 
@@ -15,33 +15,30 @@ class Arena{
         console.log(`The winner is ${winner}`);
     }
 
-    fightRound(){
+    fightRound() {
         const attacker = this.player1.health < this.player2.health ? this.player1 : this.player2;
         const defender = this.player1.health < this.player2.health ? this.player2 : this.player1;
 
         this.attack(attacker, defender);
 
-        if(defender.isAlive()){
+        if (defender.isAlive()) {
             this.attack(defender, attacker);
         }
     }
 
-    attack(attacker, defender){
+    attack(attacker, defender) {
         const attackRoll = Dice.roll();
         const defendRoll = Dice.roll();
-        // attackVal = attack_value * attack_roll
-        // defendVal = defend_strength*defend_roll
 
-        const attackDamage = attacker.attack*attackRoll;
-        const defendStrength = defender.strength*defendRoll;
+        const attackDamage = attacker.attack * attackRoll;
+        const defendStrength = defender.strength * defendRoll;
 
         const damage = attackDamage - defendStrength;
 
-        if(damage > 0){
+        if (damage > 0) {
             defender.health -= damage;
-        }else{
-            defender.health = 0;
         }
+
+        // console.log(`${attacker.name} attacks ${defender.name} for ${damage > 0 ? damage : 0} damage. ${defender.name} health: ${defender.health}`);
     }
 }
-
--- test/arena.test.js
+++ test/arena.test.js
@@ -3,37 +3,14 @@ import Player from '../src/player.js';
 import Arena from '../src/arena.js';
 import Dice from '../src/dice.js';
 
-
-
-describe('Arena', () =>{
-    it('should handle a complete fight', () => {
-        const playerA = new Player('Player A', 50, 5,10);
-        const playerB = new Player('Player B', 100, 10, 5);
-        const arena = new Arena(playerA, playerB);
-
-        // Mocking dice rolls for predictability in test
-        Dice.roll = () => 5; // attack roll
-        arena.attack(playerA, playerB);
-        expect(playerB.health).to.equal(50);
-
-        Dice.roll = () => 2; // defend roll
-        arena.attack(playerB, playerA);
-        expect(playerA.health).to.equal(20);
-
-    });
-
-    it('should correctly calculate damage and update health', () => {
+describe('Arena', () => {
+    it('should end with either playerA or playerB dead', () => {
         const playerA = new Player('Player A', 50, 5, 10);
         const playerB = new Player('Player B', 100, 10, 5);
+
         const arena = new Arena(playerA, playerB);
+        arena.startFight();
 
-        // Mocking dice rolls for predictability in test
-        Dice.roll = () => 5; // attack roll
-        arena.attack(playerA, playerB);
-        expect(playerB.health).to.equal(50);
-
-        Dice.roll = () => 2; // defend roll
-        arena.attack(playerB, playerA);
-        expect(playerA.health).to.equal(20);
+        expect(playerA.isAlive() && playerB.isAlive()).to.be.false; 
     });
 });
